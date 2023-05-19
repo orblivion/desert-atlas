7af310d6ea7fc56ffe9f6bee6298153fe09140f8
Daniel Krol <orblivion@gmail.com>
2023-05-19 14:02:53 -0400

    WIP build_region.sh

--- generate-data/build_region.sh
+++ generate-data/build_region.sh
@@ -10,7 +10,7 @@ PKG_DIR=$BUILD_DIR/pkg
 
 mkdir $PKG_DIR
 
-PBF_FILE_NAME=$REGION-latest.osm.pbf
+PBF_FILE_NAME=$REGION.osm.pbf
 PBF_FILE_TMP=$BUILD_DIR/$PBF_FILE_NAME
 # Keep pbf file (not in a temp dir). Don't want to abuse Geofabrik by
 # downloading over and over in case we need to re-run this.
@@ -23,10 +23,6 @@ SEARCH_FILE=$PKG_DIR/search.csv
 # Note the . at the end. The package will be split into mulitple files
 # and they will be numbered, starting with this prefix.
 PKG_PREFIX=$REGION.tar.gz.
-
-# Download *only if we don't have it already* (again, not to abuse Geofabrik)
-# TODO - could md5 verify that we have the right thing
-ls $PBF_FILE || (wget https://download.geofabrik.de/$REGION_PATH/$REGION-latest.osm.pbf -O $PBF_FILE_TMP && mv $PBF_FILE_TMP $PBF_FILE) || exit 1
 
 python3 extract_search.py $PBF_FILE $SEARCH_FILE
 
