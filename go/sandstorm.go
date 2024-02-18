919f7c737faa55abb14b387721d09d282c76d056
Daniel Krol <orblivion@gmail.com>
2024-02-17 23:53:25 -0500

    GetSandstormId, IsLocalDev, GetUniqueId impl/test

--- go/sandstorm.go
+++ go/sandstorm.go
@@ -7,10 +7,28 @@ import (
 
 type Permission string
 
+const HeaderLocalDevelopment = "X-Local-Development"
+const HeaderSandstormPermissions = "X-Sandstorm-Permissions"
+const HeaderSandstormId = "X-Sandstorm-User-Id"
+
 const PermissionBookmarks = Permission("bookmarks")
 const PermissionDownload = Permission("download")
 
 type Permissions []Permission
+
+type SandstormId string
+
+func IsLocalDev(r *http.Request) bool {
+	return r.Header.Get(HeaderLocalDevelopment) == "true"
+}
+
+func GetSandstormId(r *http.Request) *SandstormId {
+	sid := SandstormId(r.Header.Get(HeaderSandstormId))
+	if sid != "" {
+		return &sid
+	}
+	return nil
+}
 
 func (pp Permissions) Has(hp Permission) bool {
 	for _, p := range pp {
@@ -22,13 +40,22 @@ func (pp Permissions) Has(hp Permission) bool {
 }
 
 func SandstormPermissions(r *http.Request) Permissions {
-	if r.Header.Get("X-Local-Development") == "true" {
+	if IsLocalDev(r) {
 		return Permissions{PermissionBookmarks, PermissionDownload}
 	}
 	var ps Permissions
-	for _, strPerm := range strings.Split(r.Header.Get("X-Sandstorm-Permissions"), ",") {
+	for _, strPerm := range strings.Split(r.Header.Get(HeaderSandstormPermissions), ",") {
 		// Why not just add it. If it's invalid it just won't match anything.
 		ps = append(ps, Permission(strPerm))
 	}
 	return ps
+}
+
+func GetUniqueId(r *http.Request) *SandstormId {
+	if IsLocalDev(r) {
+		return nil
+	}
+	// Anon can't persist identity even through a page load,
+	// so we're giving up on saving tutorial modes for them.
+	return GetSandstormId(r)
 }
--- go/sandstorm_test.go
+++ go/sandstorm_test.go
@@ -6,16 +6,76 @@ import (
 	"testing"
 )
 
-func logMe(s *string) string {
+// Confirms they're both or neither nil. If neither,
+// compares the *values* of the strings.
+func cmpSidPtrs(a *SandstormId, b *SandstormId) bool {
+	if a == nil && b == nil {
+		return true
+	}
+	return *a == *b
+}
+
+func logSidPtr(s *SandstormId) string {
+	return logStrPtr((*string)(s))
+}
+
+func logStrPtr(s *string) string {
 	if s == nil {
 		return "nil"
 	}
 	return "\"" + *s + "\""
 }
 
-func TestSandstormPermissions(t *testing.T) {
-
+func TestIsLocalDev(t *testing.T) {
 	sWrong := "wrong"
+	sTrue := "true"
+	sBlank := ""
+
+	req, _ := http.NewRequest("GET", "", nil)
+	req.Header.Set("X-Local-Development", sTrue)
+	if !IsLocalDev(req) {
+		t.Errorf(`X-Local-Development = "%s" expected to return true`, sTrue)
+	}
+
+	wrongTests := [](*string){
+		nil, &sWrong, &sBlank,
+	}
+
+	for _, val := range wrongTests {
+		req, _ := http.NewRequest("GET", "", nil)
+		if val != nil {
+			req.Header.Set("X-Local-Development", *val)
+		}
+		if IsLocalDev(req) {
+			t.Errorf(`X-Local-Development = %s expected to return false`, logStrPtr(val))
+		}
+	}
+}
+
+func TestGetSandstormId(t *testing.T) {
+	sTest := "test-id"
+	sidTest := SandstormId(sTest)
+	sBlank := ""
+
+	req, _ := http.NewRequest("GET", "", nil)
+	req.Header.Set("X-Sandstorm-User-Id", sTest)
+	if got, want := GetSandstormId(req), &sidTest; !cmpSidPtrs(got, want) {
+		t.Errorf(`X-Sandstorm-User-Id = %s expected to return %s`, sTest, sidTest)
+	}
+
+	req, _ = http.NewRequest("GET", "", nil)
+	if GetSandstormId(req) != nil {
+		t.Errorf(`X-Sandstorm-User-Id = nil expected to return nil`)
+	}
+
+	req, _ = http.NewRequest("GET", "", nil)
+	req.Header.Set("X-Sandstorm-User-Id", sBlank)
+	if GetSandstormId(req) != nil {
+		t.Errorf(`X-Sandstorm-User-Id = \"%s\" expected to return nil`, sBlank)
+	}
+}
+
+func TestSandstormPermissions(t *testing.T) {
 	sTrue := "true"
 	sBookmarks := "bookmarks"
 	sDownload := "download"
@@ -33,18 +93,6 @@ func TestSandstormPermissions(t *testing.T) {
 		{
 			ssPerm:            nil,
 			localDev:          nil,
-			bookmarksExpected: false,
-			downloadExpected:  false,
-		},
-		{
-			ssPerm:            nil,
-			localDev:          &sWrong,
-			bookmarksExpected: false,
-			downloadExpected:  false,
-		},
-		{
-			ssPerm:            nil,
-			localDev:          &sBlank,
 			bookmarksExpected: false,
 			downloadExpected:  false,
 		},
@@ -111,7 +159,7 @@ func TestSandstormPermissions(t *testing.T) {
 			req.Header.Set("X-Local-Development", *tt.localDev)
 		}
 
-		errMsgPart := fmt.Sprintf(`X-Sandstorm-Permissions: %s, X-Local-Development: %s`, logMe(tt.ssPerm), logMe(tt.localDev))
+		errMsgPart := fmt.Sprintf(`X-Sandstorm-Permissions: %s, X-Local-Development: %s`, logStrPtr(tt.ssPerm), logStrPtr(tt.localDev))
 
 		if got, want := SandstormPermissions(req).Has(PermissionBookmarks), tt.bookmarksExpected; want != got {
 			t.Errorf(`%s Bookmarks permission expected: %v got: %v`, errMsgPart, want, got)
@@ -122,4 +170,50 @@ func TestSandstormPermissions(t *testing.T) {
 		}
 	}
 
+}
+
+func TestGetUniqueId(t *testing.T) {
+	sTest := "test-id"
+	sidTest := SandstormId(sTest)
+
+	tests := []struct {
+		ssId       *string
+		isLocalDev bool
+		uniqueId   *SandstormId
+	}{
+		{
+			ssId:       nil,
+			isLocalDev: false,
+			uniqueId:   nil,
+		},
+		{
+			ssId:       &sTest,
+			isLocalDev: false,
+			uniqueId:   &sidTest,
+		},
+		{
+			ssId:       nil,
+			isLocalDev: true,
+			uniqueId:   nil,
+		},
+		{
+			ssId:       &sTest,
+			isLocalDev: true,
+			uniqueId:   nil,
+		},
+	}
+
+	for _, tt := range tests {
+		req, _ := http.NewRequest("GET", "", nil)
+		if tt.ssId != nil {
+			req.Header.Set("X-Sandstorm-User-Id", *tt.ssId)
+		}
+		if tt.isLocalDev {
+			req.Header.Set("X-Local-Development", "true")
+		}
+		if got, want := GetUniqueId(req), tt.uniqueId; !cmpSidPtrs(want, got) {
+			errMsgPart := fmt.Sprintf(`X-Sandstorm-Id: %s, is local development: %v`, logStrPtr(tt.ssId), tt.isLocalDev)
+			t.Errorf(`%s UniqueId expected: %v got: %v`, errMsgPart, logSidPtr(want), logSidPtr(got))
+		}
+	}
 }
