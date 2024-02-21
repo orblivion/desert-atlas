7210c83624f0dbb20a66ad3120782683d5d3dc9e
Daniel Krol <orblivion@gmail.com>
2024-02-20 21:30:23 -0500

    go: tutorial mode POST endpoint

--- go/tutorial.go
+++ go/tutorial.go
@@ -1,12 +1,58 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
 // The purpose of this is to reset the user's tutorial mode to intro if the
 // permissions change, since there's a different set of instructions
 type TutorialType string
 
-const TutorialTypeDownloader = TutorialType("downloader")
-const TutorialTypeBookmarker = TutorialType("viewer")
-const TutorialTypeViewer = TutorialType("viewer")
+const TutorialTypeDownloader = TutorialType("TUTORIAL_TYPE_DOWNLOADER")
+const TutorialTypeBookmarker = TutorialType("TUTORIAL_TYPE_BOOKMARKER")
+const TutorialTypeViewer = TutorialType("TUTORIAL_TYPE_VIEWER")
+
+type TutorialMode string
+
+const TutorialModeIntro = TutorialMode("TUTORIAL_INTRO")
+const TutorialModeStarted = TutorialMode("TUTORIAL_STARTED")
+const TutorialModeDone = TutorialMode("TUTORIAL_DONE")
+
+type TutorialStatus struct {
+	Mode TutorialMode `json:"mode"`
+	Type TutorialType `json:"type"`
+}
+
+type TutorialFile map[SandstormUserId]TutorialStatus
+
+type TutorialUpdate struct {
+	Mode TutorialMode `json:"tutorial-mode"`
+}
+
+func (s *Server) tutorialFilePath() string {
+	return filepath.Join(s.dataDir, "tutorial.json")
+}
+
+func (s *Server) InitTutorial() error {
+	_, err := os.Stat(s.tutorialFilePath())
+
+	// No error means it exists, so nothing to initialize
+	if err == nil {
+		return nil
+	}
+
+	// os.IsNotExist error means it doesn't exist yet, initialize it
+	if os.IsNotExist(err) {
+		return os.WriteFile(s.tutorialFilePath(), []byte("{}"), 0600)
+	}
+
+	// Some other error means all bets are off, don't proceed
+	return fmt.Errorf("Unknown error accessing tutorial file")
+}
 
 func GetTutorialType(pp Permissions) TutorialType {
 	switch {
@@ -16,5 +62,75 @@ func GetTutorialType(pp Permissions) TutorialType {
 		return TutorialTypeBookmarker
 	default:
 		return TutorialTypeViewer
+	}
+}
+
+func validateTutorialMode(m TutorialMode) bool {
+	switch m {
+	case TutorialModeIntro:
+		return true
+	case TutorialModeStarted:
+		return true
+	case TutorialModeDone:
+		return true
+	default:
+		return false
+	}
+}
+
+func (s *Server) TutorialModeHandler(w http.ResponseWriter, r *http.Request) {
+	var update TutorialUpdate
+	err := json.NewDecoder(r.Body).Decode(&update)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	if !validateTutorialMode(update.Mode) {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	// TODO - this gets a bit scary race-condition-wise,
+	// but tutorial is not super important.
+	// probably move this to sqlite3 tho.
+	var tf TutorialFile
+	rFile, err := os.Open(s.tutorialFilePath())
+	if err != nil {
+		fmt.Printf("Error opening tutorial json file: %s\n", err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	if err = json.NewDecoder(rFile).Decode(&tf); err != nil {
+		fmt.Printf("Error decoding json: %s\n", err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	if userId := GetSandstormUserId(r); userId != nil {
+		tf[*userId] = TutorialStatus{
+			Mode: TutorialMode(update.Mode),
+			Type: GetTutorialType(SandstormPermissions(r)),
+		}
+
+		// Write result to temp file
+		wFile, err := os.CreateTemp("", "tutorial.*.json")
+		if err != nil {
+			fmt.Printf("Error creating temp file: %s\n", err.Error())
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		if err = json.NewEncoder(wFile).Encode(&tf); err != nil {
+			fmt.Printf("Error encoding json: %s\n", err.Error())
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		wFile.Close()
+
+		// Rename temp file to our real file
+		if err = os.Rename(wFile.Name(), s.tutorialFilePath()); err != nil {
+			fmt.Printf("Error renaming file: %s\n", err.Error())
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
 	}
 }
--- go/tutorial_test.go
+++ go/tutorial_test.go
@@ -1,24 +1,162 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"reflect"
+	"strings"
 	"testing"
 )
 
 func TestGetTutorialType(t *testing.T) {
-
 	tests := []struct {
 		perms Permissions
 		ttype TutorialType
 	}{
-		{Permissions{"bookmark", "download"}, TutorialTypeDownloader},
-		{Permissions{"bookmark"}, TutorialTypeBookmarker},
-		{Permissions{"download"}, TutorialTypeDownloader},
+		{Permissions{PermissionBookmarks, PermissionDownload}, TutorialTypeDownloader},
+		{Permissions{PermissionBookmarks}, TutorialTypeBookmarker},
+		{Permissions{PermissionDownload}, TutorialTypeDownloader},
 		{Permissions{}, TutorialTypeViewer},
 	}
 
 	for _, tt := range tests {
 		if got, want := GetTutorialType(tt.perms), tt.ttype; want != got {
 			t.Errorf(`For: %v got: %s want: %s`, tt.perms, got, want)
+		}
+	}
+}
+
+func TestValidateTutorialMode(t *testing.T) {
+	if !validateTutorialMode(TutorialModeIntro) {
+		t.Errorf(`Expected TutorialModeIntro to be valid`)
+	}
+	if !validateTutorialMode(TutorialModeStarted) {
+		t.Errorf(`Expected TutorialModeStarted to be valid`)
+	}
+	if !validateTutorialMode(TutorialModeDone) {
+		t.Errorf(`Expected TutorialModeDone to be valid`)
+	}
+	if validateTutorialMode(TutorialMode("invalid")) {
+		t.Errorf(`Expected "invalid" to be invalid`)
+	}
+}
+
+func TestTutorialInit(t *testing.T) {
+	s := initTestServer()
+	defer teardownTestServer(&s)
+
+	parts := strings.Split(s.tutorialFilePath(), "/")
+	if parts[len(parts)-1] != "tutorial.json" {
+		t.Fatal("tutorial path not as expected")
+	}
+
+	if _, err := os.Stat(s.tutorialFilePath()); err == nil || !os.IsNotExist(err) {
+		t.Fatal("tutorial path shouldn't exist yet")
+	}
+
+	if s.InitTutorial() != nil {
+		t.Fatalf("error initializing tutorial")
+	}
+
+	if content, err := os.ReadFile(s.tutorialFilePath()); string(content) != "{}" || err != nil {
+		t.Fatalf("tutorial file should be initialized to {}. content: %s err: %v", string(content), err)
+	}
+
+	testData := "{1:2}"
+	// write other data
+	if err := os.WriteFile(s.tutorialFilePath(), []byte(testData), 0600); err != nil {
+		t.Fatalf("error writing test data %s", err.Error())
+	}
+	if s.InitTutorial() != nil {
+		t.Fatalf("error initializing tutorial (second time)")
+	}
+	content, err := os.ReadFile(s.tutorialFilePath())
+	if err != nil {
+		t.Fatalf("error reading test data %s", err.Error())
+	}
+	if string(content) != testData {
+		t.Fatal("tutorial file initialization shouldn't overwrite existing data")
+	}
+}
+
+func TestGetTutorialModeHandler(t *testing.T) {
+	s := initTestServer()
+	defer teardownTestServer(&s)
+
+	if s.InitTutorial() != nil {
+		t.Fatalf("error initializing tutorial")
+	}
+
+	testUserId := "test-user-id"
+	testSUserId := SandstormUserId(testUserId)
+
+	tests := []struct {
+		hasUserId             bool
+		hasBookmarkPermission bool
+		hasValidMode          bool
+		statusCode            int
+		want                  TutorialFile
+	}{
+		// with invalid mode, return 400 regardless of user id and permission
+		{hasUserId: true, hasBookmarkPermission: true, hasValidMode: false, statusCode: http.StatusBadRequest, want: TutorialFile{}},
+		{hasUserId: true, hasBookmarkPermission: false, hasValidMode: false, statusCode: http.StatusBadRequest, want: TutorialFile{}},
+		{hasUserId: false, hasBookmarkPermission: true, hasValidMode: false, statusCode: http.StatusBadRequest, want: TutorialFile{}},
+		{hasUserId: false, hasBookmarkPermission: false, hasValidMode: false, statusCode: http.StatusBadRequest, want: TutorialFile{}},
+
+		// with no user id and valid mode, regardless of permission, return 200 but no entry created
+		{hasUserId: false, hasBookmarkPermission: true, hasValidMode: true, statusCode: http.StatusOK, want: TutorialFile{}},
+		{hasUserId: false, hasBookmarkPermission: false, hasValidMode: true, statusCode: http.StatusOK, want: TutorialFile{}},
+
+		// with user id and valid mode, permission determines type of entry created
+		{hasUserId: true, hasBookmarkPermission: false, hasValidMode: true, statusCode: http.StatusOK, want: TutorialFile{
+			testSUserId: {
+				Mode: TutorialModeStarted,
+				Type: TutorialTypeViewer,
+			},
+		}},
+		{hasUserId: true, hasBookmarkPermission: true, hasValidMode: true, statusCode: http.StatusOK, want: TutorialFile{
+			testSUserId: {
+				Mode: TutorialModeStarted,
+				Type: TutorialTypeBookmarker,
+			},
+		}},
+	}
+
+	for _, tt := range tests {
+		if os.WriteFile(s.tutorialFilePath(), []byte("{}"), 0600) != nil {
+			t.Fatal("error resetting tutorial file")
+		}
+
+		var mode TutorialMode
+		if tt.hasValidMode {
+			mode = TutorialModeStarted
+		} else {
+			mode = TutorialMode("invalid")
+		}
+		requestBody := []byte(fmt.Sprintf(`{"tutorial-mode": "%s"}`, mode))
+		req := httptest.NewRequest(http.MethodPost, "/app-go/tutorial-mode", bytes.NewBuffer(requestBody))
+		if tt.hasUserId {
+			req.Header.Set("X-Sandstorm-User-Id", testUserId)
+		}
+		if tt.hasBookmarkPermission {
+			req.Header.Set("X-Sandstorm-Permissions", "bookmarks")
+		}
+		w := httptest.NewRecorder()
+		s.TutorialModeHandler(w, req)
+
+		var got TutorialFile
+		file, _ := os.Open(s.tutorialFilePath())
+		json.NewDecoder(file).Decode(&got)
+
+		if tt.statusCode != w.Code {
+			t.Errorf("for: %+v status code want: %+v got: %+v", tt, tt.statusCode, w.Code)
+		}
+		if !reflect.DeepEqual(tt.want, got) {
+			t.Errorf("for: %+v tutorial file entry want: %+v got: %+v", tt, tt.want, got)
 		}
 	}
 }
